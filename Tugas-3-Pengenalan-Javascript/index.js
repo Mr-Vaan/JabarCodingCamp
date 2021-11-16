6230c300e32d90433a36c63243543c460feda17e
IvanChriana <ivandhebel@gmail.com>
2021-11-16 21:39:52 +0700

    Belajar - Pengenalan Javascript

---
+++ Tugas-3-Pengenalan-Javascript/index.js
@@ -0,0 +1,47 @@
+// ------------------ SOAL KE 1 --------------------------
+ // jawaban soal 1
+var pertama = "saya";
+var kedua = "senang";
+var ketiga = "belajar";
+var keempat = "JAVASCRIPT";
+console.log(pertama, kedua, ketiga, keempat);
+
+// ------------------ SOAL KE 2 --------------------------
+// jawaban soal 2
+var number1 = "10";
+var number2 = "2";
+var number3 = "4";
+var number4 = "6";
+console.log(number1 % number2 + number3 * number4);
+
+// ------------------ SOAL KE 3 --------------------------
+// jawaban soal 3
+var kalimat = 'wah javascript itu keren sekali'; 
+
+var kataPertama = kalimat.substring(0, 3); 
+var kataKedua = kalimat.substring(4, 14); 
+var kataKetiga = kalimat.substring(15, 18); 
+var kataKeempat = kalimat.substring(19, 24); 
+var kataKelima = kalimat.substring(25, 31); 
+
+console.log('Kata Pertama: ' + kataPertama); 
+console.log('Kata Kedua: ' + kataKedua); 
+console.log('Kata Ketiga: ' + kataKetiga); 
+console.log('Kata Keempat: ' + kataKeempat); 
+console.log('Kata Kelima: ' + kataKelima);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
